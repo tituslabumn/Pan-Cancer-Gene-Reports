6404551e90e2d61b95a4fcc1fc309bcdc6e2fc4f
tsharding <taylor.s.harding@gmail.com>
2019-08-15 10:50:17 -0500

    backup by tsharding

--- gnomAD_query.R
+++ gnomAD_query.R
@@ -23,18 +23,18 @@ library("RSelenium")
 URL_gnomAD <- paste0("https://gnomad.broadinstitute.org/gene/",GOI_ENSG,"?dataset=gnomad_r2_1_non_cancer")
 # https://gnomad.broadinstitute.org/gene/ENSG00000145555?dataset=gnomad_r2_1_non_cancer
 
-# eCaps <- list(
-#   chromeOptions = 
-#     list(prefs = list(
-#       "profile.default_content_settings.popups" = 0L,
-#       "download.prompt_for_download" = FALSE,
-#       "download.default_directory" = "/OUTPUT"
-#     )
-#     )
-# )
+eCaps <- list(
+  chromeOptions =
+    list(prefs = list(
+      "profile.default_content_settings.popups" = 0L,
+      "download.prompt_for_download" = FALSE,
+      #"download.default_directory" = "/OUTPUT"
+    )
+    )
+)
 
 # selenium is the service name of other container on user defined network per docker-compose file
-remDr <- remoteDriver(remoteServerAddr = "selenium" ,port = 4444L, browser = "chrome", )#extraCapabilities = eCaps) 
+remDr <- remoteDriver(remoteServerAddr = "selenium" ,port = 4444L, browser = "chrome", extraCapabilities = eCaps) 
 cat("\tOpening connection via remote driver\n")
 remDr$open(silent = TRUE)
 # navigate to GOI gnomAD page
@@ -55,8 +55,7 @@ webElem_dl_button$sendKeysToElement(list(key = "down_arrow"))
 # remDr$screenshot(display = TRUE)
 Sys.sleep(3)
 cat("\tClick download button\n")
-webElem_dl_button$clickElement()
-remDr$screenshot(display = TRUE)
+# webElem_dl_button$clickElement()
 cat("\tClose driver connection\n")
 remDr$close()
 
