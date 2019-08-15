0cfa36d9b037bd75ccfe15e5528f04b593065621
tsharding <taylor.s.harding@gmail.com>
2019-08-15 12:31:04 -0500

    backup by tsharding

--- gnomAD_query.R
+++ gnomAD_query.R
@@ -4,13 +4,13 @@ cat("#####################################################################\n")
 cat("############# Retrieving all gnomAD variants for GOI ################\n")
 cat("#####################################################################\n\n\n")
 
-# cat("Cheching for linked pcgr_selenium_chrome docker container...\n\n")
-# 
-# if("selenium-file" %in% list.files(path = "/")){
-#   cat("\tlink confirmed :]\n\n") #assumes selenium container was not stopped after pcgr container was brought up
-# }else{
-#   stop("\tCan't fined shared volume from selenium container")
-# }
+cat("Cheching for linked pcgr_selenium_chrome docker container...\n\n")
+
+if("selenium-file" %in% list.files(path = "/")){
+  cat("\tlink confirmed :]\n\n") #assumes selenium container was not stopped after pcgr container was brought up
+}else{
+  stop("\tCan't fined shared volume from selenium container")
+}
 
 cat("Retriving gnomAD non-cancer variant file via RSelenium web scraper\n\n")
 
@@ -26,9 +26,9 @@ URL_gnomAD <- paste0("https://gnomad.broadinstitute.org/gene/",GOI_ENSG,"?datase
 eCaps <- list(
   chromeOptions =
     list(prefs = list(
-      #"profile.default_content_settings.popups" = 0L,
-      "download.prompt_for_download" = FALSE,
-      "download.default_directory" = "~/"
+      "profile.default_content_settings.popups" = 0L,
+      "download.prompt_for_download" = FALSE#,
+      #"download.default_directory" = "/home/seluser/Downloads"
     )
     )
 )
@@ -41,21 +41,19 @@ remDr$open(silent = TRUE)
 cat("\tNavigating to gnomAD browser GOI page(non-cancer):\n")
 cat("\t\t",URL_gnomAD,"\n")
 remDr$navigate(URL_gnomAD)
-Sys.sleep(3)
+Sys.sleep(3) #wait for page to load
 cat("\t\tPage title:\n")
 print(remDr$getTitle())
       # remDr$screenshot(display = TRUE)
 #   find download button (xpath found by exploring element in chromium)
 cat("\tFinding download button\n")
 webElem_dl_button <- remDr$findElement(using = "xpath","//*[@id='root']/div/div/div[2]/div/div[5]/section/div[2]/button")
-Sys.sleep(3)
 # click download button
 cat("\tMouse over download button\n")
 webElem_dl_button$sendKeysToElement(list(key = "down_arrow"))
-Sys.sleep(3)
 cat("\tClick download button\n")
 webElem_dl_button$clickElement()
 cat("\tClose driver connection\n")
 remDr$close()
 
-  print(list.files(path = "/selenium-file/"))
+print(list.files(path = "/selenium-file/"))
